b8df17b9054d1a80481c1127df8262dcbb28e153
Vincent <makokhavomondi@gmail.com>
2023-05-07 19:52:43 +0300

    Make asset hold the information of one asset

--- asset.py
+++ asset.py
@@ -4,10 +4,21 @@ import pandas as pd
 
 
 class Asset:
-    def __init__(self):
+    def __init__(self, ticker, market="US", index=None):
+        self.market = market
+        self.index = index
         self.ss = StockSymbol("fb6844dd-04c4-4a1e-9596-6b451a0461b3")
 
-    def find(self, asset_name=None, market="US", index=None):
+        symbol_list = self.ss.get_symbol_list(market=market, index=index)
+        info = dict(pd.DataFrame(symbol_list).set_index('symbol').loc[ticker, :])
+        info["ticker"] = ticker
+
+        self.info = info
+
+    def find(self, asset_name=None, market=None, index=None):
+        market = market if market != None else self.market
+        index = index if index != None else self.index
+
         if(asset_name == None):
             return self.ss.get_symbol_list(market=market, index=index)
         else:
@@ -18,7 +29,7 @@ class Asset:
             distances.append(self.__sequence_similarity(asset.lower(), asset_name.lower()))
 
         highest_similarity = distances.index(max(distances))
-        print(highest_similarity)
+
         return market_data.iloc[highest_similarity, :]
 
     
