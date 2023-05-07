e214d94d60eb72972699f8167cc25a087e55e17d
Vincent <makokhavomondi@gmail.com>
2023-05-07 23:45:22 +0300

    Add some comments on asset module

--- asset.py
+++ asset.py
@@ -18,7 +18,10 @@ class Asset:
 
     def get(self):
         return self.info
-
+    
+    # Takes in a name (e.g micros) and returns the name of the company
+    # that matches that the most. It is used for searching company
+    # tickers using their names
     def find(self, asset_name=None, market=None, index=None):
         market = market if market != None else self.market
         index = index if index != None else self.index
@@ -36,7 +39,8 @@ class Asset:
 
         return market_data.iloc[highest_similarity, :]
 
-    
+    # Matches two strings and provides a value that indicates
+    # how similar the two values are    
     def __sequence_similarity(self, str1, str2):
         matcher = SequenceMatcher(None, str1, str2)
         return matcher.ratio()
