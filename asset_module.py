f22a2fdd2f47b9b50a001fcbbbaadb9ef76e2466
Vincent <makokhavomondi@gmail.com>
2023-05-07 14:05:58 +0300

    Add the asset module
    This will enable user to search the tickers for the asset listed in most of the markets in the world

---
+++ asset_module.py
@@ -0,0 +1,24 @@
+from stocksymbol import StockSymbol
+from difflib import SequenceMatcher
+import pandas as pd
+
+
+class Asset:
+    def __init__(self):
+        self.ss = StockSymbol("fb6844dd-04c4-4a1e-9596-6b451a0461b3")
+
+    def find(self, asset_name, market="US", index=None):
+        market_data = pd.DataFrame(self.ss.get_symbol_list(market=market, index=index))
+
+        distances = []
+        for asset in market_data["shortName"]:
+            distances.append(self.__sequence_similarity(asset.lower(), asset_name.lower()))
+
+        highest_similarity = distances.index(max(distances))
+        print(highest_similarity)
+        return market_data.iloc[highest_similarity, :]
+
+    
+    def __sequence_similarity(self, str1, str2):
+        matcher = SequenceMatcher(None, str1, str2)
+        return matcher.ratio()
