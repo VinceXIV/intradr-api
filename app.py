cce11ae106794d147c6aff67dd9ef098baaae667
Vincent <makokhavomondi@gmail.com>
2023-05-09 13:22:04 +0300

    Enable specifying market, index, and number of items when getting assets

--- app.py
+++ app.py
@@ -12,11 +12,16 @@ CORS(app, support_credentials=True)
 @app.route('/assets', methods=['GET'])
 @cross_origin(support_credentials=True)
 def index():
-    market = request.get_json()['market']
-    index = request.get_json()['index']
+    market = request.args['market']
+    index = request.args['index'] if request.args['index'] != "N/A" else None
+    limit = int(request.args["limit"])
 
     ss = StockSymbol(sym)
-    result = ss.get_symbol_list(market=market, index=index)
+    result = ss.get_symbol_list(market=market, index=index)[:limit]
 
     return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run()
 
