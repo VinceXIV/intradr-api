bc52cf1709c8eba610c19fa709685b8984085873
Vincent <makokhavomondi@gmail.com>
2023-05-15 11:40:19 +0300

    Enable evaluation of expressions in api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from stocksymbol import StockSymbol
 from ssm import *
+from expression import Expression
 
 
 
@@ -20,6 +21,15 @@ def index():
     result = ss.get_symbol_list(market=market, index=index)[:limit]
 
     return jsonify(result)
+
+@app.route('/evaluate', methods=['GET'])
+@cross_origin(support_credentials=True)
+def index():
+    str_ = request.args['expression']
+    assets = request.args['assets']
+    expr = Expression(assets=assets, str_expression=str_)
+
+    return jsonify(expr.evaluate())
 
 
 if __name__ == "__main__":
