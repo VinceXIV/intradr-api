86f05567df211b1289a830cebe0614f8b6dee85d
Vincent <makokhavomondi@gmail.com>
2023-05-15 11:29:59 +0300

    Make the matrix functions robust when values in stead of matrices are passed in the matrixfunctions module, call method

--- matrixfunctions.py
+++ matrixfunctions.py
@@ -5,17 +5,17 @@ from sympy.parsing.sympy_parser import T
 def call(function_name, argument_array, variable_dict):
     argument_array = get_argument_values(argument_array, variable_dict)
     if(function_name == "_avg"):
-        return pd.Series(list(argument_array[0])).mean()
+        return pd.Series(list([argument_array[0]])).mean()
     elif(function_name == "_max"):
-        return pd.Series(list(argument_array[0])).max()
+        return pd.Series(list([argument_array[0]])).max()
     elif(function_name == "_min"):
-        return pd.Series(list(argument_array[0])).min()
+        return pd.Series(list([argument_array[0]])).min()
     elif(function_name == "_std"):
-        return pd.Series(list(argument_array[0])).std()
+        return pd.Series(list([argument_array[0]])).std()
     elif(function_name == "_var"):
-        return pd.Series(list(argument_array[0])).var()
+        return pd.Series(list([argument_array[0]])).var()
     elif(function_name == "_quantile"):
-        return pd.Series(list(argument_array[0])).quantile(argument_array[1])
+        return pd.Series(list([argument_array[0]])).quantile(argument_array[1])
     elif(function_name == "_mmult"):
         return parse_expr("{m1}*{m2}".format(m1 = argument_array[0], m2=argument_array[1]), evaluate=True, transformations=T[:11])
     elif(function_name == "_transpose"):
