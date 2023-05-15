af63c0ea1dd4043c87afbaacca4aff4dd20dbad8
Vincent <makokhavomondi@gmail.com>
2023-05-15 11:54:50 +0300

    Update comments on methods in the expressiontree module

--- expressiontree.py
+++ expressiontree.py
@@ -6,8 +6,8 @@ import re
 # Takes in an expression such as "x^(2y) + 3x + 7" and returns an array of expressions
 # such as ['2*y', 'x**(2*y)', '3*x', '3*x + x**(2*y) + 7'], which basically means we will
 # have to solve 2*x first, then x**(2*x) where we will just substitute the solution we got
-# from solving 2*x. After that we solve for 3*x then finally '3*x + x**(2*y) + 7'. In the
-# final stage, we will already have the solution for 3*x and x**(2*y), so at this point
+# from solving 2*x in the x**(2*x). After that we solve for 3*x then finally '3*x + x**(2*y) + 7'.
+# In the final stage, we will already have the solution for 3*x and x**(2*y), so at this point
 # after substituting with the solution, the expression might look something like 1 + 2 + 7
 def get_ordered_operations(str_expr):
     parsed_str = parse_expr(str_expr, evaluate=False, transformations="all")
