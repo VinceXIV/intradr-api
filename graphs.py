416a336e9ae230dc32ed59075989fe9fdd7c6d72
Vincent <makokhavomondi@gmail.com>
2023-05-16 15:08:43 +0300

    Rename "graph" function to "plot". Also, improve on its variable naming

--- graphs.py
+++ graphs.py
@@ -5,14 +5,14 @@ import mpld3
 import matplotlib.pyplot as plt
 from backdated import get_backdated_values
 
-def graph(graph, variables, expression_array, assets, backdate_period=30, figsize=(9, 4)):
+def plot(graph_type, variables, expression_array, assets, backdate_period=30, figsize=(9, 4)):
     df = get_backdated_values(
         expression_array = expression_array,
         assets = assets,
         backdate_period = backdate_period)
 
     fig = plt.figure(figsize = figsize)
-    if(graph == "line"):
+    if(graph_type == "line"):
         for v in variables:
             plt.plot(df.index, df[v])
             plt.title("line graph")
