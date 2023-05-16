6b0c2df2df74cf2458ca6409e632eb789fd73290
Vincent <makokhavomondi@gmail.com>
2023-05-17 00:28:55 +0300

    Process dictionary before sending to frontend

---
+++ apputilities.py
@@ -0,0 +1,19 @@
+def process_data_dict(dict_):
+    '''
+    Receives data in the form {0: <variable_name>: [1], 1: <variable_name>: [3]} and transforms it
+    to {period: [0, 1], values: [3, 1]}. Also stringifies floats.
+    Note the switch. that's because 1 represent the nearest
+    '''
+    result = {}
+    for period in dict_:
+        p = len(dict_) - period
+        for variable in dict_[period]:
+            value = dict_[period][variable]
+
+            if(variable not in result):
+                result[variable] = []
+
+            result[variable].append((p, str(value)))
+
+    return result
+
