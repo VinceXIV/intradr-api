a9c4ebc921b6e0574f98fd9a1e0e24f05575f50b
Vincent <makokhavomondi@gmail.com>
2023-05-22 12:55:13 +0300

    Make expression.get_innermost_functions() able to recognize when some function arguments are arrays

--- expression.py
+++ expression.py
@@ -162,8 +162,8 @@ class Expression:
         innermost_functions = []
 
         for arg_count in range(min_arg_count, max_arg_count+1):
-            r = r"".join([",\s*\w+\s*" for i in range(arg_count)]) + r"\)"
-            regex = r"\w+\(\s*\w+\.?\w*\s*" + r
+            r = r"".join([",\s*[\[\]]?\s*\w+\s*[\[\]]?\s*" for i in range(arg_count)]) + r"\)"
+            regex = r"\w+\(\s*\[?\s*\w+\.?\w*\s*\]?\s*" + r
             innermost_functions.extend(re.findall(regex, str_expression))
 
         return innermost_functions 
